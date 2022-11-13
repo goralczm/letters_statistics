894417757c468680152b827ff3549b139198e6ee
goralczm <goralczm@gmail.com>
2022-11-14 00:03:18 +0100

    Futher optimizing script, added new file to test

--- slowa_statystyki.py
+++ slowa_statystyki.py
@@ -1,6 +1,7 @@
 import collections
 import string
 import json
+import os
 
 alphabet = string.ascii_lowercase
 vowel = [*'aąeęiouóy']
@@ -14,13 +15,16 @@ def occurances(letterBase):
             alphabetDict[letter] += 1
     return alphabetDict
 
+
 # Sorts dictionary by values from lowest to highest
 def sortDictByVal(dict):
     return {key: value for key, value in sorted(dict.items(), key=lambda item: item[1])}
 
+
 # Returns how many letters are in given dictionary containing letter occurances
 def totalLetters(occurancesDict):
     return sum(occurancesDict.values())
+
 
 # Returns how many vowels are in given dictionary. containing letter occurances
 def vowelAmount(occurancesDict):
@@ -30,78 +34,120 @@ def vowelAmount(occurancesDict):
             vowels += occurancesDict[letter]
     return vowels
 
+
 # Return list of two items, first is the most commong letter, second is how many of them are in given occurances dictionary
 def mostCommonLetter(occurancesDict):
     sortedDict = sortDictByVal(occurancesDict)
     lastKey = list(sortedDict.keys())[-1]
     return [lastKey, occurancesDict[lastKey]]
 
+
 # Returns occurances of given letter from occurances dictionary
 def letterOccurance(occurancesDict, letter):
     return occurancesDict[letter]
 
+
 def percentAmount(a, b):
     return round((a/b)*100, 2)
 
-def importJsonData(fileName):
-    with open(fileName, "r", encoding='utf-8') as jsonFile:
-        data = json.load(jsonFile)
-        return data
 
-with open("slowa.txt", "r", encoding='utf-8') as wordsFile:
-    content = wordsFile.readlines()
-    
-    statisticsFile = importJsonData('statistics.json')
-    occurances = statisticsFile['occurances'][0]
+def readStatistics(file_name):
+    try:
+        with open(f'{file_name}_statistics.json', "r", encoding='utf-8') as jsonFile:
+            print('Reading json file')
+            data = json.load(jsonFile)
+            return data
+    except FileNotFoundError:
+        print('Trying to save statistics')
+        return saveStatistics(file_name)
 
-    #occurances = collections.defaultdict(int)
-    
-    maxLenght = 0
-    longestWords = []
-    mostRecursiveLine = ['', 0, '']
-    
-    for line in content:
-        cleanLine = line.strip()
+def saveStatistics(file_name):
+    try:
+        with open(f'{file_name}.txt', 'r', encoding='utf-8') as wordsFile:
+            with open(f'{file_name}_statistics.json', 'w', encoding='utf-8') as words_stats_file:
+
+                print('Saving statistics...')
+
+                statistics = {}
+                file_content = wordsFile.readlines()
+
+                occurances = collections.defaultdict(int)
+
+                max_line_len = 0
+                longest_words = []
+
+                print('Counting letters...')
+
+                for line in file_content:
+                    clean_line = line.strip()
+
+                    for letter in clean_line:
+                        occurances[letter] += 1
+                    
+                    wordLen = len(clean_line)
+                    if (max_line_len < wordLen):
+                        max_line_len = wordLen
+                        longest_words = [clean_line]
+                    elif (max_line_len == wordLen):
+                        longest_words.append(clean_line)
+
+                print('Calculating statistics...')
+
+                total_letters = totalLetters(occurances)
+        
+                most_common_letter = mostCommonLetter(occurances)
+                most_common_letter_percent = percentAmount(most_common_letter[1], total_letters)
+        
+                vowel_amount = vowelAmount(occurances)
+                vowel_percent = percentAmount(vowel_amount, total_letters)
+        
+                consonant_amount = total_letters - vowel_amount
+                consonant_percent = 100 - vowel_percent
+
+                print('Combining statistics...')
                 
-        # Letter occurances
-        #for letter in cleanLine:
-        #    occurances[letter] += 1
-        
-        # Longest word
-        wordLen = len(cleanLine)
-        if (maxLenght < wordLen):
-            maxLenght = wordLen
-            longestWords = [cleanLine]
-        elif (maxLenght == wordLen):
-            longestWords.append(cleanLine)
-        
-        # Most recursive letters word
-        lettersCounter = collections.Counter(list(cleanLine))
-        maxLetter = max(lettersCounter, key=lettersCounter.get)
-        currentRecursive = [cleanLine, lettersCounter[maxLetter]]
-        if (mostRecursiveLine[1] < currentRecursive[1]):
-            mostRecursiveLine[0] = cleanLine
-            mostRecursiveLine[1] = currentRecursive[1]
-            mostRecursiveLine[2] = maxLetter
-    
-    totalLetters = totalLetters(occurances)
-    
-    mostCommonLetter = mostCommonLetter(occurances)
-    mostCommonLetterPercent = percentAmount(mostCommonLetter[1], totalLetters)
-    
-    vowelAmount = vowelAmount(occurances)
-    vowelPercent = percentAmount(vowelAmount, totalLetters)
-    
-    consonantAmount = totalLetters - vowelAmount
-    consonantPercent = 100 - vowelPercent
-    
-    print(occurances)
+                statistics['occurances'] = occurances
+                statistics['most_common_letter'] = [most_common_letter, most_common_letter_percent]
+                statistics['total_letters'] = total_letters
+                statistics['vowels'] = [vowel_amount, vowel_percent]
+                statistics['consonants'] = [consonant_amount, consonant_percent]
+                statistics['longest_words'] = [len(longest_words), longest_words]
+
+                print('Saving statistics...')
+
+                json_data = json.dumps(statistics, indent=4)
+                words_stats_file.write(json_data)
+            print('Statistics has been saved successfully')
+            return readStatistics(file_name)
+    except FileNotFoundError:
+        print('File does not exist')
+        return None
 
 
-    #print(f'Total letters: {totalLetters}')
-    #print(f'Most common letter is "{mostCommonLetter[0]}" with {mostCommonLetter[1]} occurances (~{mostCommonLetterPercent}%)')
-    #print(f'There are {vowelAmount} vowels (~{vowelPercent}%)')
-    #print(f'There are {consonantAmount} consonants (~{consonantPercent}%)')
-    #print(f'This are the {len(longestWords)} longest words with {maxLenght} letters each')
-    #print(f'  First five longest words are "{"\", \"".join(longestWords[0:5])}"')
-    #print(f'Word with most recursive letter is "{mostRecursiveLine[0]}" with {mostRecursiveLine[1]} occurances of letter "{mostRecursiveLine[2]}"')
+def printFileStatistics(file_name, force_new_save = False):
+    if (force_new_save):
+        statistics = saveStatistics(file_name)
+    else:
+        statistics = readStatistics(file_name)
+
+    total_letters = statistics['total_letters']
+    most_common_letter = statistics['most_common_letter']
+    vowel_amount = statistics['vowels'][0]
+    vowel_percent = statistics['vowels'][1]
+    consonant_amount = statistics['consonants'][0]
+    consonant_percent = statistics['consonants'][1]
+    longest_words = statistics['longest_words']
+
+    print(f'\nTotal letters: {total_letters}')
+    print(f'Most common letter is "{most_common_letter[0][0]}" with {most_common_letter[0][1]} occurances (~{most_common_letter[1]}%)')
+    print(f'There are {vowel_amount} vowels (~{vowel_percent}%)')
+    print(f'There are {consonant_amount} consonants (~{consonant_percent}%)')
+    print(f'There are {longest_words[0]} longest words with {len(longest_words[1][0])} letters each')
+    print(f'  First five longest words are "{", ".join(longest_words[1][0:5])}"')
+
+#files_to_test = ['slowa', 'words_alpha', 'pan-tadeusz']
+files_to_test = ['pan-tadeusz']
+for file in files_to_test:
+    print(f'------------------------ {file}.txt ------------------------')
+    printFileStatistics(file, True)
+    print(f'------------------------ {file}.txt ------------------------\n')
